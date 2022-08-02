af14c933b30470c6a728982d9eb5f78c72faacbb
Admin <Admin@DESKTOP-KHB48LB>
2022-08-02 17:03:14 +0530

    Updated Code for webclient

--- happyfreshweb/src/happyfreshweb/happyfreshwebclient.java
+++ happyfreshweb/src/happyfreshweb/happyfreshwebclient.java
@@ -169,26 +169,74 @@ public class happyfreshwebclient
 			Thread.sleep(2000);
 			driver.findElement(By.id("phoneNumberShort")).sendKeys("8802823334");
 			Thread.sleep(2000);
-			driver.findElement(By.id("email")).sendKeys("demo2@gmail.com");
+			driver.findElement(By.id("email")).sendKeys("demo23@gmail.com");
 			Thread.sleep(2000);
 			driver.findElement(By.id("password")).sendKeys("Rajput@321");
 			Thread.sleep(2000);
 			driver.findElement(By.id("password-confirm")).sendKeys("Rajput@321");
 			Thread.sleep(3000);
 			driver.findElement(By.id("submitButton")).click();
+		}
 			
-			Thread.sleep(3000);
-			driver.findElement(By.xpath("//*[@id='app']/div[2]/div/div[1]/div[2]/div/div[1]/a")).click();
-			Thread.sleep(2000);
-			driver.findElement(By.id("input-delivery-address")).sendKeys("Greater Jakarta");
+			@Test(priority=6)
+			public void order() throws InterruptedException
+			{
+				driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+				Thread.sleep(3000);
+				driver.findElement(By.xpath("//*[@id='app']/div[2]/div/div[1]/div[2]/div/div[1]/a")).click();
+				Thread.sleep(2000);
+				driver.findElement(By.id("input-delivery-address")).sendKeys("Greater Jakarta");
+				Thread.sleep(5000);
+				driver.findElement(By.id("prediction-address-1")).click();
+				Thread.sleep(2000);
+				js.executeScript("window.scrollBy(0,1000)");
+				Thread.sleep(3000);
+				driver.findElement(By.xpath("//*[@id='app']/div[2]/div/div[1]/div[2]/div/div[2]/div/input")).sendKeys("Test");
+				Thread.sleep(4000);
+				driver.findElement(By.xpath("//*[@id='app']/div[2]/div/div[1]/div[3]/div[2]/div/div[2]")).click();
+				
+				Thread.sleep(10000);
+		/*	WebElement radio=	driver.findElement(By.xpath("//*[@id='app']/div[2]/div/div/div[2]/div[3]/div[3]/label"));
 			Thread.sleep(5000);
-			driver.findElement(By.id("prediction-address-1")).click();
-			Thread.sleep(2000);
-			js.executeScript("window.scrollBy(0,1000)");
-			Thread.sleep(3000);
-			driver.findElement(By.id("placeholder")).sendKeys("Test");
+		     WebElement radio =	ws.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='app']/div[2]/div/div/div[2]/div[3]/div[3]")));
+			Thread.sleep(5000);
+			
+		    js.executeScript("arguments[0].scrollIntoView(true);", radio);	
+			Thread.sleep(4000);
+		   radio.click();
+			
+			
+			if(radio.isSelected())
+			{
+			System.out.println("Selected");
+			}
+			else
+			{
+				System.out.println("Not selected");
+			}   */
+			
+				Actions action = new Actions(driver);
+				action.sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+				
+				
+				Thread.sleep(5000);
+			   WebElement ts =	driver.findElement(By.xpath("//*[@id='app']/div[2]/div/div/div[3]/button"));
+			   ts.click();
+			   Thread.sleep(7000);
+			   driver.findElement(By.id("button-submit")).click();
+			   Thread.sleep(5000);
+				driver.findElement(By.id("payment-method--cod")).click();
+
+				Thread.sleep(4000);
+				js.executeScript("window.scrollBy(0,1000)");
+				Thread.sleep(4000);
+				driver.findElement(By.id("button-submit")).click();
+				Thread.sleep(4000);
+				driver.findElement(By.id("replacement--submit")).click();
+			   
+			}
 			
 			
 		}
 
-}
+
